0f8f4ea2136eb2c0952e43a39feaa2e57b85b601
Sarang <mahajansarang1997@gmail.com>
2025-09-04 14:26:32 +0530

    Update Delivery Form by deliver no Save API

---
+++ Eunoia-UM_API/Controllers/VoucherUpdateController.cs
@@ -0,0 +1,62 @@
+﻿using Eunoia_UM_API.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using Eunoia_UM_API.Model;
+using System.Net;
+
+namespace Eunoia_UM_API.Controllers
+{
+    public class VoucherUpdateController : Controller
+    {
+        Api_CommonResponse response = new Api_CommonResponse();
+
+        [HttpPost]
+        [Route("api/SaveDeliveryDetails")]
+        public Api_CommonResponse SaveDeliveryDetails([FromBody] VoucherUpdateModel voucherUpdateModel)
+        {
+            try
+            {
+                SqlParameter[] param = new SqlParameter[15];
+                param[0] = new SqlParameter("@iDeliveryNo", voucherUpdateModel.iDeliveryNo ?? (object)DBNull.Value);
+                param[1] = new SqlParameter("@sConsignmntNo", voucherUpdateModel.sConsignmntNo ?? (object)DBNull.Value);
+                param[2] = new SqlParameter("@iOrderTyp", voucherUpdateModel.iOrderTyp ?? (object)DBNull.Value);
+                param[3] = new SqlParameter("@dtDoDate", voucherUpdateModel.dtDoDate ?? (object)DBNull.Value);
+                param[4] = new SqlParameter("@sNewConsignmntNo", (object?)voucherUpdateModel.sNewConsignmntNo ?? DBNull.Value);
+                param[5] = new SqlParameter("@dtConsignmntDt", voucherUpdateModel.dtConsignmntDt ?? (object)DBNull.Value);
+                param[6] = new SqlParameter("@dGrossWt", voucherUpdateModel.dGrossWt ?? (object)DBNull.Value);
+                param[7] = new SqlParameter("@dNetWt", voucherUpdateModel.dNetWt ?? (object)DBNull.Value);
+                param[8] = new SqlParameter("@dInvoicWt", voucherUpdateModel.dInvoicWt ?? (object)DBNull.Value);
+                param[9] = new SqlParameter("@sInvoiceNo", (object?)voucherUpdateModel.sInvoiceNo ?? DBNull.Value);
+                param[10] = new SqlParameter("@dtInvoicDt", voucherUpdateModel.dtInvoicDt ?? (object)DBNull.Value);
+                param[11] = new SqlParameter("@iShortg", voucherUpdateModel.iShortg ?? (object)DBNull.Value);
+                param[12] = new SqlParameter("@sCityName", voucherUpdateModel.cityName ?? (object)DBNull.Value);
+                param[13] = new SqlParameter("@sCatName", voucherUpdateModel.catName ?? (object)DBNull.Value);
+                param[14] = new SqlParameter("@sproductName", voucherUpdateModel.productName ?? (object)DBNull.Value);
+
+                DataSet ds = DBOperation.FillDataSet("USP_UpdateDeliveryDetails", param);
+                if (ds != null && ds.Tables != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    response.responseCode = 0;
+                    response.message = ds.Tables[0].Rows[0]["Message"].ToString();
+                    response.statusCode = (int)HttpStatusCode.OK;
+                }
+                else
+                {
+                    response.responseCode = 1;
+                    response.message = $"We are facing server issue at the moment please try again...";
+                    response.statusCode = -1;
+                    response.data = null;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return response;
+        }
+    }
+}
+    
---
+++ Eunoia-UM_API/Model/VoucherUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Eunoia_UM_API.Model
+{
+    public class VoucherUpdateModel
+    {
+        public string iDeliveryNo { get; set; }        // Delivery No
+        public string sConsignmntNo { get; set; }      // Existing Consignment No (hidden/old)
+        public int? iOrderTyp { get; set; }            // Order Type
+        public DateTime? dtDoDate { get; set; }        // Delivery Date
+        public string sNewConsignmntNo { get; set; }   // New CNote No
+        public DateTime? dtConsignmntDt { get; set; }  // New CNote Date
+        public decimal? dGrossWt { get; set; }         // Gross Weight
+        public decimal? dNetWt { get; set; }           // Net Weight
+        public decimal? dInvoicWt { get; set; }        // Invoice Weight
+        public string sInvoiceNo { get; set; }         // Invoice No
+        public DateTime? dtInvoicDt { get; set; }      // Invoice Date
+        public decimal? iShortg { get; set; }          // Shortage
+        public string? cityName { get; set; }      // Destination
+        public string? catName { get; set; }     // Category
+        public string? productName { get; set; }
+    }
+}
